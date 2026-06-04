ca87978a856e66328e4c4a1d69dffc36145cf2e5
Dagobert-78 <152921793+Dagobert-78@users.noreply.github.com>
2026-06-04 02:14:11 +0200

    VFS_update.bat

--- VFS_update.bat
+++
@@ -1,4 +0,0 @@
-set "VFS_Last_Release_build=61"
-set "VFS_Last_Release_version=4.8.1.0"
-set "VFS_Last_Release_download=https://github.com/Dagobert-78/VFS/releases/download/4.8.1.0/Verification.Fichiers.Systeme.v4.8.1.0.cmd"
-
